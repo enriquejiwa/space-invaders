a7a4d42977143f4664763dad8930f11896ac174d
enriq <enriquejiwa@gmail.com>
2021-11-15 20:38:09 +0100

    Added Alien class, refactored to use inheritance.

---
+++ source/creature.py
@@ -0,0 +1,64 @@
+"""Module containing the class Creature and its inherited, Player and Alien.
+"""
+import pygame
+
+
+class Creature():
+    """Parent class that defines the shared attributes and methods.
+    """
+
+    def __init__(self, x_coord: int, y_coord: int,
+                 x_change: int = 0, y_change: int = 0):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.x_change = x_change
+        self.y_change = y_change
+        self.image = None
+
+    def show(self, screen: pygame.Surface):
+        """Displays the creature onto the screen at coordinates determined by
+        the attributes.
+
+        Args:
+            screen (pygame.Surface): The surface where the creature will be
+            displayed.
+        """
+        if self.image:
+            screen.blit(self.image, (self.x_coord, self.y_coord))
+
+
+class Player(Creature):
+    """Class that corresponds to the player, inherits from Creature.
+    """
+
+    def __init__(self):
+        super().__init__(350, 700)
+        self.set_image()
+
+    def set_image(self):
+        """Sets the sprite of the player, uses the sprites.png file.
+        """
+        img = pygame.image.load("assets/sprites.png").convert()
+        img = img.subsurface((1, 49, 16, 8))
+        self.image = pygame.transform.scale(img, (48, 24))
+
+
+class Alien(Creature):
+    """Class that corresponds to the enemies, inherits from Creature.
+    """
+    sprite_coord = {0: (1, 1), 1: (19, 1), 2: (37, 1),
+                    3: (1, 11), 4: (19, 11), 5: (37, 11)}
+
+    def __init__(self, x_coord: int, y_coord: int, style: int):
+        super().__init__(x_coord, y_coord)
+        self.set_image(style)
+
+    def set_image(self, style: int):
+        """Sets the sprite of the player, uses the sprites.png file.
+
+        Args:
+            style (int): The version on the sprite.
+        """
+        img = pygame.image.load("assets/sprites.png").convert()
+        img = img.subsurface((*self.sprite_coord[style], 16, 8))
+        self.image = pygame.transform.scale(img, (48, 24))
--- source/game.py
+++ source/game.py
@@ -1,33 +1,59 @@
-"""[summary]
+"""Module containing the Game class.
 """
 import pygame
-from . import player
+from . import creature
 
 
 class Game():
+    """Class that manages the execution of the game.
+    """
+
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((700, 800))
         self.set_window_settings()
         self.running = True
-        self.player = player.Player()
+        self.player = creature.Player()
+        self.enemies = []
+        self.create_enemies()
 
     def set_window_settings(self):
+        """Sets the window information, the caption and icon.
+        """
         pygame.display.set_caption("Space Invaders")
         icon = pygame.image.load("assets/icon.png")
         pygame.display.set_icon(icon)
 
     def event_loop(self):
+        """Checks for the events and utilizes this information as necessary.
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
 
     def draw(self):
+        """Fills and updates the screen with all the objects.
+        """
         self.screen.fill((0, 0, 0))
         self.player.show(self.screen)
+        for enemy in self.enemies:
+            enemy.show(self.screen)
         pygame.display.update()
 
     def start(self):
+        """Contains the infinite loop that keeps the game running.
+        """
         while self.running:
             self.event_loop()
             self.draw()
+
+    def create_enemies(self):
+        """Helper method for the initilitzation of all the enemies.
+        """
+        y_coord = 100
+        for style in range(6):
+            x_coord = 75
+            for _ in range(11):
+                self.enemies.append(creature.Alien(x_coord, y_coord, style//2))
+                x_coord += 50
+            y_coord += 40
